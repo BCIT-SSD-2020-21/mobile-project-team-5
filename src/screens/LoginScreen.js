d57c28e30ac943981955c5edd7756aa086be87ff
Gökay Abay <abaygokay@gmail.com>
2021-03-29 19:03:18 -0700

    before expo eject

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import React, { useState } from "react"
+import {
+  Container,
+  Header,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+} from "native-base"
 
-const LoginScreen = ({navigation}) => {
-  return(
-    <View>
-      <TouchableOpacity onPress={() => {
-        navigation.navigate('Register')
-      }}>
-        <Text>To Register</Text>
-      </TouchableOpacity>
-    </View>
+import { View, Text, StyleSheet } from "react-native"
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler"
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  return (
+    <Container>
+      <Content>
+        <Form>
+          <Item fixedLabel>
+            <Label>Email</Label>
+            <Input onChange={(text) => setEmail(text)} />
+          </Item>
+          <Item fixedLabel last>
+            <Label>Password</Label>
+            <Input />
+          </Item>
+          <Button block light>
+            <Text>Sign In</Text>
+          </Button>
+        </Form>
+      </Content>
+    </Container>
   )
 }
 
-export default LoginScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#fff",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: "white",
+  },
+})
+
+export default LoginScreen
